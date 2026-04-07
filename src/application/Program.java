41ff10d2131fa1f9c63dae9f74e413406be6b6a9
Felipe Schwartz <felipeschwartz@gmail.com>
2026-04-07 17:16:15 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,10 @@ package application;
 
 import java.time.LocalDate;
 import java.util.Scanner;
+
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +14,9 @@ public class Program {
         Scanner sc = new Scanner(System.in);
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller.toString());
 
         sc.close();
