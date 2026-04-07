3cb3a4d44bbb2451491dcf081c1add427907708e
Felipe Schwartz <felipeschwartz@gmail.com>
2026-04-07 17:00:01 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
+
+import java.time.LocalDate;
 import java.util.Scanner;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        $
+        Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
+        System.out.println(seller.toString());
 
         sc.close();
     }
