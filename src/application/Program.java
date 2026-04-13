aa404dd623fa6b96adef16e9afa13e3bd8ce7cc3
Felipe Schwartz <felipeschwartz@gmail.com>
2026-04-13 17:33:48 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,6 +30,12 @@ public class Program {
             System.out.println(obj.toString());
         }
 
+        System.out.println("\n==== TEST 3: seller findAll ====");
+        list = sellerDao.findAll();
+        for (Seller obj : list) {
+            System.out.println(obj.toString());
+        }
+
         sc.close();
     }
 }
